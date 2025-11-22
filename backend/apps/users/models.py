4de1f687c66546ff021a99bc05c92e44b700b758
GiornoGiovanaJoJo <57575835+GiornoGiovanaJoJo@users.noreply.github.com>
2025-11-23 00:59:53 +0300

    Fix import in User model

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models.AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
